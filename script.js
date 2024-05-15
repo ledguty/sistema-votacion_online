c7be8d0dc5939dc74e526fc8e6dfb72846a92711
ledguty <ledguty@gmail.com>
2024-05-15 14:40:06 -0600

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-let votosSi = 0;
-let votosNo = 0;
-let votosAbstenerse = 0;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const botonSi = document.getElementById('si');
-const botonNo = document.getElementById('no');
-const botonAbstenerse = document.getElementById('abstenerse');
-const resultado = document.getElementById('resultado');
+// **NO INCLUIR LA API KEY EN EL LADO DEL CLIENTE!**
+const firebaseConfig = {
+  apiKey: "AIzaSyBa629rrtbevtO3WkAEAm8FQU33dvGekNs", // Reemplaza con tu apiKey (solo en el backend)
+  authDomain: "votacion-87298.firebaseapp.com",
+  projectId: "votacion-87298",
+  storageBucket: "votacion-87298.appspot.com",
+  messagingSenderId: "107840795713",
+  appId: "1:107840795713:web:7feca98f10251226c482c0"
+};
 
-function actualizarResultado() {
-  resultado.innerHTML = `Resultados:<br>Sí: ${votosSi}<br>No: ${votosNo}<br>Abstenerse: ${votosAbstenerse}`;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+function registrarUsuario() {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const usuario = document.getElementById("usuario").value;
+  const contraseña = document.getElementById("contraseña").value;
+
+  // Validar los datos (agregar validaciones según sea necesario)
+
+  createUserWithEmailAndPassword(auth, usuario, contraseña) // Asumiendo que "usuario" es el email
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      set(ref(database, 'users/' + user.uid), {
+        nombre: nombre,
+        usuario: usuario
+      })
+      .then(() => {
+        alert("Usuario registrado exitosamente!");
+      })
+      .catch((error) => {
+        console.error("Error al guardar en la base de datos:", error);
+        alert("Error al registrar usuario");
+      });
+
+    })
+    .catch((error) => {
+      console.error("Error al crear usuario:", error);
+      alert("Error al registrar usuario");
+    });
+
+  return false;
 }
-
-botonSi.addEventListener('click', () => {
-  votosSi++;
-  actualizarResultado();
-});
-botonNo.addEventListener('click', () => {
-  votosNo++;
-  actualizarResultado();
-});
-botonAbstenerse.addEventListener('click', () => {
-  votosAbstenerse++;
-  actualizarResultado();
-});
